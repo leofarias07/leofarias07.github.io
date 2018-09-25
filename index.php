964f69b6547ded21fdac89a87eaffeb97a658400
Leonardo <leo.farias.05@gmail.com>
2018-09-25 15:03:17 -0300

    finalizando slider

--- index.php
+++ index.php
@@ -12,6 +12,47 @@
      <link rel="stylesheet" href="css/style.css">
      <link href="//maxcdn.bootstrapcdn.com/bootstrap/3.3.0/css/bootstrap.min.css" rel="stylesheet" id="bootstrap-css">
 
+     <!-- Add the slick-theme.css if you want default styling -->
+<link rel="stylesheet" type="text/css" href="//cdn.jsdelivr.net/gh/kenwheeler/slick@1.9.0/slick/slick.css"/>
+<!-- Add the slick-theme.css if you want default styling -->
+<link rel="stylesheet" type="text/css" href="//cdn.jsdelivr.net/gh/kenwheeler/slick@1.9.0/slick/slick-theme.css"/>
+  <link rel="stylesheet" type="text/css" href="css/slick.css">
+  <link rel="stylesheet" type="text/css" href="css/slick-theme.css">
+  <style type="text/css">
+ 
+   
+
+    .slider {
+        width: 100%;
+       
+    }
+
+    .slick-slide {
+      margin: 0px 20px;
+    }
+
+ 
+
+    .slick-prev:before,
+    .slick-next:before {
+      color: black;
+    }
+
+
+    .slick-slide {
+      transition: all ease-in-out .3s;
+      opacity: .2;
+    }
+    
+    .slick-active {
+     opacity: 1;
+    }
+
+    .slick-current {
+      opacity: 1;
+    }
+  </style>
+
      <script>
        /* Set the width of the side navigation to 250px */
         function openNav() {
@@ -205,34 +246,42 @@
       <div class="container">
         <div class="row">
             <div class="col-md-12">
-              <div style="background-color: #08478c; color: #fff; padding: 10px; font-size: 22px; font-weight: bolder;">Blogs</div>
+              <div style="background-color: #08478c; color: #fff; padding: 15px; font-size: 22px; font-weight: bolder;">Blogs</div>
             </div>
         
           </div>
           <div class="row">
-             <div class="col-md-12 mt-3">
-                <div id="carouselExampleControls" class="carousel slide" data-ride="carousel">
-                  <div class="carousel-inner">
-                    <div class="carousel-item active">
-                      <img class="d-block w-100" src="https://via.placeholder.com/1105x100" class="img-fluid" alt="First slide">
-                    </div>
-                    <div class="carousel-item">
-                      <img class="d-block w-100" src="https://via.placeholder.com/1105x100" class="img-fluid" alt="Second slide">
-                    </div>
-                    <div class="carousel-item">
-                      <img class="d-block w-100" src="https://via.placeholder.com/1105x100" class="img-fluid" alt="Third slide">
-                    </div>
-                  </div>
-                  <a class="carousel-control-prev" href="#carouselExampleControls" role="button" data-slide="prev">
-                    <span class="carousel-control-prev-icon" aria-hidden="true"></span>
-                    <span class="sr-only">Previous</span>
-                  </a>
-                  <a class="carousel-control-next" href="#carouselExampleControls" role="button" data-slide="next">
-                    <span class="carousel-control-next-icon" aria-hidden="true"></span>
-                    <span class="sr-only">Next</span>
-                  </a>
+            
+             <div class="regular slider">
+                <div>
+                  <p style="height: 100px; background:#eaeaea;padding: 15px; ">Pré Candidatura de Eduado Sá Avança e novas parcerias são firmadas</p>
+                  <span class="label label-slider">Felipe Sousa</span>
                 </div>
+               <div>
+                  <p style="height:  100px; background:#eaeaea;padding: 15px; ">Pré Candidatura de Eduado Sá Avança e novas parcerias são firmadas</p>
+                  <span class="label label-slider">Felipe Sousa</span>
                 </div>
+               
+              <div>
+                  <p style="height: 100px; background:#eaeaea;padding: 15px; ">Pré Candidatura de Eduado Sá Avança e novas parcerias são firmadas</p>
+                  <span class="label label-slider">Felipe Sousa</span>
+                </div>
+              <div>
+                  <p style="height: 100px; background:#eaeaea;padding: 15px; ">Pré Candidatura de Eduado Sá Avança e novas parcerias são firmadas</p>
+                  <span class="label label-slider">Felipe Sousa</span>
+                </div>
+              <div>
+                  <p style="height:  100px; background:#eaeaea;padding: 15px; ">Pré Candidatura de Eduado Sá Avança e novas parcerias são firmadas</p>
+                  <span class="label label-slider">Felipe Sousa</span>
+                </div>
+              <div>
+                  <p style="height:  100px; background:#eaeaea;padding: 15px; ">Pré Candidatura de Eduado Sá Avança e novas parcerias são firmadas</p>
+                  <span class="label label-slider">Felipe Sousa</span>
+                </div>
+   
+
+              </div>
+               
             </div>
         </div>
   
@@ -568,5 +617,22 @@
     <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
     <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.3/umd/popper.min.js" integrity="sha384-ZMP7rVo3mIykV+2+9J3UJ46jBk0WLaUAdn689aCwoqbBJiSnjAK/l8WvCWPIPm49" crossorigin="anonymous"></script>
     <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/js/bootstrap.min.js" integrity="sha384-ChfqqxuZUCnJSK3+MXmPNIyE6ZbWh2IMqE241rYiqJxyMiZ6OW/JmZQ5stwEULTy" crossorigin="anonymous"></script>
+
+
+      <script src="https://code.jquery.com/jquery-2.2.0.min.js" type="text/javascript"></script>
+  <script src="js/slick.js" type="text/javascript" charset="utf-8"></script>
+  <script type="text/javascript">
+    $(document).on('ready', function() {
+ 
+      $(".regular").slick({
+        dots: true,
+        infinite: true,
+        slidesToShow: 3,
+        slidesToScroll: 3
+      });
+   
+    });
+</script>
+
   </body>
 </html>
